084782c17d185250af4b7449584bdff15fbe16bc
Julien Sailly <juliensailly76@gmail.com>
2025-09-23 18:32:51 +0200

    feat: Q11 - getPokemons() method

--- pokedemo/src/app/poke-api.ts
+++ pokedemo/src/app/poke-api.ts
@@ -10,4 +10,8 @@ export class PokeAPI {
   constructor(http: HttpClient) {
     this.http = http
   }
+
+  getPokemons(limit: number = 100) {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
+  }
 }
