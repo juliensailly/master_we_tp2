fc83b39c688ef78861b58d48c6b34c163557cfbc
Julien Sailly <juliensailly76@gmail.com>
2025-09-23 18:34:32 +0200

    feat: Q12

--- pokedemo/src/app/app-module.ts
+++ pokedemo/src/app/app-module.ts
@@ -7,6 +7,7 @@ import { MyComponent } from './my-component/my-component';
 import { FormsModule } from '@angular/forms';
 import { FilterPokemonPipePipe } from './filter-pokemon--pipe-pipe';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { PokeAPI } from './poke-api';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatSlideToggleModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    PokeAPI
   ],
   bootstrap: [App]
 })
