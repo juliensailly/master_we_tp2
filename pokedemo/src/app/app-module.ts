dec87f61053f0fd57209c740cb9800a7849ab103
Julien Sailly <juliensailly76@gmail.com>
2025-10-02 10:50:47 +0200

    feat: Q17 working - material components added

--- pokedemo/src/app/app-module.ts
+++ pokedemo/src/app/app-module.ts
@@ -1,6 +1,7 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -10,6 +11,12 @@ import { FilterPokemonPipePipe } from './filter-pokemon--pipe-pipe';
 import { PokeAPI } from './poke-api';
 import { PokemonDetails } from './pokemon-details/pokemon-details';
 import { PokemonCommunication } from './pokemon-communication';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,13 @@ import { PokemonCommunication } from './pokemon-communication';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
