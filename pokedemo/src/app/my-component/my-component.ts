faa94bf2fefc4d44d9bc8de3b04a5b00be0f8db6
Julien Sailly <juliensailly76@gmail.com>
2025-10-20 14:49:25 +0200

    feat: adding api access to load the dropdown and search

--- pokedemo/src/app/my-component/my-component.ts
+++ pokedemo/src/app/my-component/my-component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { PokemonCommunication } from '../pokemon-communication';
+import { PokeAPI } from '../poke-api';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,19 +14,19 @@ export class MyComponent implements OnInit, OnDestroy {
   filterQuery: string = '';
   selectedPokemonId: string = '-1';
   private subscription: Subscription = new Subscription();
+  isLoadingPokemons: boolean = true;
+  errorLoadingPokemons: string = '';
 
-  // Liste de Pokemons
-  pokemons = [
-    new Pokemon('1', 'Bulbasaur'),
-    new Pokemon('2', 'Charmander'),
-    new Pokemon('3', 'Squirtle'),
-    new Pokemon('4', 'Pikachu'),
-    new Pokemon('5', 'Eevee'),
-  ];
+  pokemons: Pokemon[] = [];
 
-  constructor(private pokemonCommunicationService: PokemonCommunication) { }
+  constructor(
+    private pokemonCommunicationService: PokemonCommunication,
+    private pokeApiService: PokeAPI
+  ) { }
 
   ngOnInit() {
+    this.loadPokemonsList();
+
     this.subscription.add(
       this.pokemonCommunicationService.selectedPokemonId$.subscribe(
         (pokemonId: string) => {
@@ -35,16 +36,40 @@ export class MyComponent implements OnInit, OnDestroy {
     );
   }
 
+  loadPokemonsList() {
+    this.isLoadingPokemons = true;
+    this.errorLoadingPokemons = '';
+
+    this.pokeApiService.getPokemons(151).subscribe({
+      next: (response: any) => {
+        this.pokemons = response.results.map((pokemon: any, index: number) => {
+          const pokemonId = pokemon.url.split('/').filter((part: string) => part).pop();
+          return new Pokemon(pokemonId, this.capitalizeFirstLetter(pokemon.name));
+        });
+        this.isLoadingPokemons = false;
+      },
+      error: (error) => {
+        this.errorLoadingPokemons = 'Erreur lors du chargement de la liste des Pokémons';
+        this.isLoadingPokemons = false;
+        console.error('Erreur lors du chargement des Pokémons:', error);
+      }
+    });
+  }
+
+  private capitalizeFirstLetter(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
   displaySelectedPokemon() {
-    const selected = this.pokemons.find(p => p.id === this.selectedPokemonId)
+    const selected = this.pokemons.find(p => p.id === this.selectedPokemonId);
     if (selected) {
-      console.log('Pokemon sélectionné:', selected.name)
+      console.log('Pokemon sélectionné:', selected.name, '(ID:', selected.id, ')');
     } else {
-      console.log('Aucun Pokemon sélectionné')
+      console.log('Aucun Pokemon sélectionné');
     }
 
     this.pokemonCommunicationService.setSelectedPokemonId(this.selectedPokemonId);
